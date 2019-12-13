ce1fb14a0fdcde451987efd73fa87650d02b3cb2
Chandra Pratap <chandra_pratap@epam.com>
2019-12-13 15:58:29 +0530

    replaced var wuth const and formated code

--- task-1/question-1.js
+++ task-1/question-1.js
@@ -1,17 +1,18 @@
-//question: 
+//question:
 // 1)Write a   program which reads a string from standard input reverses the string and writes it to the standard output.
-// a)The program should run in a   
+// a)The program should run in a
 // stand-by mode and should not be terminated after reversing the first string.
 
-
 // solution:
-var stdin = process.stdin;
-stdin.on('data',(data)=>{
-    if(data.toString() === 'exit\r\n') process.exit();
-    else
-    {
-    const output = data.toString().split('').reverse().join('');
+const stdin = process.stdin;
+stdin.on("data", data => {
+  if (data.toString() === "exit\r\n") process.exit();
+  else {
+    const output = data
+      .toString()
+      .split("")
+      .reverse()
+      .join("");
     console.log(output);
-    }
-})
-
+  }
+});
--- task-1/question-2.js
+++ task-1/question-2.js
@@ -1,14 +1,14 @@
-var rl = require('readline');
-var fs = require('fs');
-var rs = fs.createReadStream('./sample.csv');
-var ws = fs.createWriteStream('output.txt');
+const rl = require("readline");
+const fs = require("fs");
+const rs = fs.createReadStream("./sample.csv");
+const ws = fs.createWriteStream("output.txt");
 
-var rli = rl.createInterface({
-    input: rs,
-    output: ws
-})
+const rli = rl.createInterface({
+  input: rs,
+  output: ws
+});
 
-rli.on('line',(line)=>{
-    ws.write(line,'UTF8');
-    ws.write('\r\n','UTF8');
-})
+rli.on("line", line => {
+  ws.write(line, "UTF8");
+  ws.write("\r\n", "UTF8");
+});
